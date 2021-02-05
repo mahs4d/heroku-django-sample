2be7ddcd33d3938be92afce3fc43b437e1857b82
Mahdi Sadeghi <mail2mahsad@gmail.com>
2021-02-05 22:30:40 +0330

    fixed media static serve

--- heroku_sample/urls.py
+++ heroku_sample/urls.py
@@ -5,5 +5,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
